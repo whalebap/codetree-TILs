d84fa0090079dbcfb271ecd1cc12ac088a2b91c3
whalebap <59642933+whalebap@users.noreply.github.com>
2024-09-16 12:16:06 +0900

    Title: 원소 10개의 합, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240916/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+    Scanner sc = new Scanner(System.in);
+
+    int sum = 0;
+    
+    for(int i=0; i<10; i++){
+        sum += sc.nextInt();
+    }
+
+    System.out.println(sum);
+    }
+}
