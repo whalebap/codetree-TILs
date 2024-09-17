b87e41d261aa05c1751eabe305ba1e3221738cbc
whalebap <59642933+whalebap@users.noreply.github.com>
2024-09-17 11:58:20 +0900

    Title: 코로나 메뉴얼2, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240917/코로나 메뉴얼2/covid-manual2.java
+++ 240917/코로나 메뉴얼2/covid-manual2.java
@@ -22,7 +22,7 @@ public class Main {
 
         String msg = "";
         for(int i=0; i<arr.length; i++){
-            if(arr[i] <=2){
+            if(arr[0] >=2){
                 msg = "E";
             }
 
