7f91445e87c97069c863fd5b14aa11cc6370c97a
whalebap <59642933+whalebap@users.noreply.github.com>
2025-01-08 20:41:32 +0900

    Title: 사칙연산 함수, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250108/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printCalculate(int a, char o, int c){
+
+        int answer = 0;
+        boolean isCalculateStr = true;
+        switch(o){
+            case '+' : answer = a + c;
+            break;
+            case '-' : answer = a - c;
+            break;
+            case '*' : answer = a * c;
+            break;
+            case '/' : answer = a / c;
+            default : isCalculateStr = false;
+                   
+        }
+
+        if(isCalculateStr){
+            System.out.println(String.valueOf(a) + " " + 
+            String.valueOf(o) + " " + String.valueOf(c) + " = " 
+            + String.valueOf(answer));
+        } else {
+            System.out.println("False");
+        }
+
+        
+        
+    }
+
+    public static void main(String[] args) {
+        // Please write your code here.
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        char o = sc.next().charAt(0);
+        int c = sc.nextInt();
+
+
+        
+        printCalculate(a, o, c);
+        
+        
+    }
+}
