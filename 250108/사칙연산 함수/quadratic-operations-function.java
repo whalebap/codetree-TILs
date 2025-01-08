1dc4c2f2320189798ea44f8279e6b7096e10e0b8
whalebap <59642933+whalebap@users.noreply.github.com>
2025-01-08 20:43:10 +0900

    Title: 사칙연산 함수, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 250108/사칙연산 함수/quadratic-operations-function.java
+++ 250108/사칙연산 함수/quadratic-operations-function.java
@@ -14,6 +14,7 @@ public class Main {
             case '*' : answer = a * c;
             break;
             case '/' : answer = a / c;
+            break;
             default : isCalculateStr = false;
                    
         }
